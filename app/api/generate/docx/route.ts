ef165129ecdc87e50c16aafcecac0eac564a9b14
Noé Campo <noecampo@delosintelligence.fr>
2025-04-27 22:26:23 +0100

    fhiez

--- app/api/generate/docx/route.ts
+++ app/api/generate/docx/route.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 // src/app/api/generate/docx/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import * as docx from "docx";
--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 // src/app/api/generate/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
