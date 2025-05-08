820d97d8277467e2b7a0cbc20f6627c83b5a181f
Noé Campo <noecampo@delosintelligence.fr>
2025-05-08 14:23:31 +0200

    jofez

--- components/layout/home.tsx
+++ components/layout/home.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+
 "use client";
 
 import { useState, useEffect } from "react";
