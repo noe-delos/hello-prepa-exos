acbd22f5e277bc92aa677da78f3084e4d37fa5aa
Noé Campo <noecampo@delosintelligence.fr>
2025-06-23 12:33:15 +0200

    hifez

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@
 const nextConfig = {
   serverExternalPackages: ["pdfkit", "docx", "pdf-parse", "pdf-to-img"],
   experimental: {
+    serverComponentsExternalPackages: [
+      "pdfkit",
+      "docx",
+      "pdf-parse",
+      "pdf-to-img",
+    ],
     serverActions: {
       bodySizeLimit: "100mb",
     },
